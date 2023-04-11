37e99fa4f220fb47d3fce15192912a2cbbb5e0bf
Divinelewis <divinitysoft0@gmail.com>
2023-04-11 17:56:14 +0100

    my git commit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-let o = ["C is fun", "Python is cool", "JavaScript is amazing"]
-for (let i = 0; i < o.length; i++) {
-  console.log(o[i]);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
