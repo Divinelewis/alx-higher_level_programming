b9b4201263a5c8f75132919355ca777745ae20f5
Divinelewis <divinitysoft0@gmail.com>
2023-04-11 17:14:35 +0100

    my git commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const args = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+const args = process.argv.split(2);
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
