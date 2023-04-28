6e3b793dc9ea1d971215b66abcbcf8ea0c8a4b4e
Divinelewis <divinitysoft0@gmail.com>
2023-04-28 20:02:09 +0100

    worked on the file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,13 @@
 """A script that
 (a) fetches https://intranet.hbtn.io/status using the urlib package
 """
-
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
